9fbbcdc8312d6b61cc4a7f80e02f7b46183cafbe
YogeshVedpathak3777 <yogesh.vedpathak@yash.com>
2020-11-29 23:08:09 +0530

    Policy claim Application

---
+++ TestAppAuthAndAuthorizeUsers/Controllers/RoleController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace TestAppAuthAndAuthorizeUsers.Controllers
+{
+    public class RoleController : Controller
+    {
+        RoleManager<IdentityRole> roleManager;
+
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        [Authorize(Policy= "readpolicy")]
+        public IActionResult Index()
+        {
+            var roles = roleManager.Roles.ToList();
+            return View(roles);
+        }
+
+        [HttpGet]
+        [Authorize(Policy = "writepolicy")]
+        public ActionResult Create()
+        {
+            return View(new IdentityRole());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(IdentityRole role)
+        {
+            await roleManager.CreateAsync(role);
+            return RedirectToAction("Index");
+        }
+    }
+}
